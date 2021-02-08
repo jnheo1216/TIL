4a90be14f9d5931e9760c66b395692be6d870455
jnheo1216 <hjeionyng94@gmail.com>
2021-02-08 23:51:24 +0900

    learn flutter navigator push

---
+++ flutter/flutter_example/flutter_navigator_push_demo/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(NavigatorPushDemo());
+
+
+class NavigatorPushDemo extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Navigator Push Demo',
+      home: MainPage(),
+    );
+  }
+}
+
+
+class MainPage extends StatelessWidget {
+  _showNextPage(BuildContext context) => Navigator.push(
+    context, MaterialPageRoute(builder: (context) => NextPage()));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigator 기본 데모'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () => _showNextPage(context),
+          child: Text('다음 화면으로 이동'),
+        ),
+      ),
+    );
+  }
+}
+
+
+class NextPage extends StatelessWidget {
+  _backToMainPage(BuildContext context) => Navigator.pop(context);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('다음페이지'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () => _backToMainPage(context),
+          child: Text('돌아가기'),
+        ),
+      ),
+    );
+  }
+}
+
+
